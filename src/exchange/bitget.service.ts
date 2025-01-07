64e042a702de79f0f9db9c304ff02c42ddd2b8a3
coolmirth35 <coolmirth35@gmail.com>
2025-01-07 11:16:13 +0000

    lastest

--- src/exchange/bitget.service.ts
+++ src/exchange/bitget.service.ts
@@ -28,8 +28,30 @@ export class BitgetService {
         volume: coins.quoteVolume,
         askPrice: coins.askPr,
         bidPrice: coins.askPr,
-      })     
+      })
     });
     return prices
   }
+  /**
+   * @function Checks if the trade is worth it
+   * @returns Number expected profit
+   */
+
+  checkEstimatedProfit(): Number {
+    // check send to buy_from exchange gas fee (if applicable)
+    // check send to sell_at exchnage gas fee
+    // check send back to base if applicable
+    // add all that and less from calculated difference 
+    return 0
+  };
+
+  checkExchangeStatus() {
+    
+  }
+
+  // check if worth it (still figuring that out)
+  // check if withdrawable in both exchanges buy_from and sell_at
+  // if buy_from is not base exchange withdraw from base exchange to buy_from exchange trading account 
+  // do spot market swap 
+  // send to sell_at exchange and do spot market swap to usdt if base exchange is not sell_at exchange send to base exchange
 }
